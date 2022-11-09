b3fddab6d631b45a212321f364538b3ff1553c3c
lucasluan01 <lucas.l.santos0204@gmail.com>
2022-11-09 00:22:57 -0300

    app criado

---
+++ startto.dev/chat/android/app/src/main/kotlin/br/com/lucas/chat/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.lucas.chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ startto.dev/chat/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Container(),
+    );
+  }
+}
